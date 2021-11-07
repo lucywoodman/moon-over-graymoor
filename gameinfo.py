c0da7309a04be7437bd1c205b3755ee21e443a3a
Lucy Woodman <lucyjwoodman@gmail.com>
2021-11-07 07:08:49 +0000

    feat: create game class to handle game introduction

---
+++ gameinfo.py
@@ -0,0 +1,9 @@
+class GameInfo():
+
+    def __init__(self, game_title):
+        self.title = game_title
+
+    def welcome(self):
+        print("-----------------------------")
+        print(f"Welcome to {self.title}")
+        print("-----------------------------")
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-
 from area import Area
 from character import Character
+from gameinfo import GameInfo
+
+mog_game = GameInfo("Moon Over Graymoor")
+mog_game.welcome()
 
 town_square = Area("Town Square")
 town_square.set_description("A light mist drifts from the loamy sky above you. The drizzle quickly freezes into glass in the streets. In the middle of the small town’s square, Graymoor’s blacksmith, George Gilly, lies face down in the snow, still, and ashen.")
