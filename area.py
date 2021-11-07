b0269ae01bf57e859a5524b41ec323b59447e721
Lucy Woodman <lucyjwoodman@gmail.com>
2021-11-07 08:20:48 +0000

    feat: add the ability to link areas together and move around

--- area.py
+++ area.py
@@ -7,6 +7,7 @@ class Area():
     def __init__(self, area_name):
         self.name = area_name
         self.description = None
+        self.linked_areas = {}
 
     # Set the description of the area
     def set_description(self, area_description):
@@ -24,8 +25,24 @@ class Area():
     def get_name(self):
         return self.name
 
+    # Connect to other areas
+    def link_area(self, area_to_link, direction):
+        self.linked_areas[direction] = area_to_link
+
     # Describe the area in the game
     def describe(self):
-        print(f"\n{self.name}")
+        print(f"{self.name}")
         print("-" * 80)
         print(f"{self.description}\n")
+        print("**List of area items or points of interest will go here**\n")
+        for direction in self.linked_areas:
+            area = self.linked_areas[direction]
+            print(f"- {area.get_name()} is to the [{direction}]")
+        print("-" * 80)
+
+    def move(self, direction):
+        if direction in self.linked_areas:
+            return self.linked_areas[direction]
+        else:
+            print("You can't go that way")
+            return self
--- run.py
+++ run.py
@@ -6,15 +6,32 @@ from character import Character
 # Create the game title
 mog_game = GameInfo("Moon Over Graymoor")
 mog_game.welcome()
+mog_game.backstory()
 
 # Create the chapter title
 chap_one = Chapter("one")
 chap_one.title()
 
 # Create the areas
-town_square = Area("Town Square")
+town_square = Area("The Town Square")
 town_square.set_description("A light mist drifts from the loamy sky above you. The drizzle quickly freezes into glass in the streets. In the middle of the small town’s square, Graymoor’s blacksmith, George Gilly, lies face down in the snow, still, and ashen.")
-town_square.describe()
 
-oswald = Character("Oswald the Coroner", "An impatient, very finely dressed man.")
-oswald.describe()
+gillys_house = Area("George Gilly's House")
+gillys_house.set_description("The doors and windows hang open, and the curtains shiver in the frigid wind. Snow has begun to creep onto the sills, and over the threshold, into the dark and empty home. As you enter, you find the floorboards in the kitchen have been pried up, and on the table next to them sits a small, artless lockbox.")
+
+town_square.link_area(gillys_house, "north")
+gillys_house.link_area(town_square, "south")
+
+current_area = town_square
+alive = True
+
+while alive == True:
+    print("\n")
+    current_area.describe()
+    
+    command = input("What would you like to do?\n> ")
+
+    if command in ["north", "south", "east", "west"]:
+        current_area = current_area.move(command)
+    else:
+        print("Please type a direction")
