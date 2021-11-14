d5467d41dc9a9035037b635db6a5da9916f36bc4
Lucy Woodman <lucyjwoodman@gmail.com>
2021-11-14 07:07:15 +0000

    feat: connect Google API and test

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('moon_over_graymoor')
+
+login = SHEET.worksheet('login')
+data = login.get_all_values()
+print(data)
+
 from gameinfo import GameInfo
 from chapter import Chapter
 from area import Area
